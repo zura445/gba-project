09936a40566d853431d551d6cb3486a29241629a
Zura Tetrashvili <z.tetrashvili@vendoo.ge>
2023-12-26 02:02:57 -0800

    add animation

--- app/page/main.tsx
+++ app/page/main.tsx
@@ -2,22 +2,45 @@ import Image from "next/image";
 
 const Main = () => {
   return (
-    <div className="w-full m-auto flex items-center justify-center h-screen">
-      <body className="font-sans antialiased">
-        <div className="group relative overflow-hidden">
+    <body className="font-sans antialiased">
+      <div className="grid grid-cols-2 gap-4">
+        <div className="group relative overflow-hidden row-span-2">
           <Image
             src="/home.jpg"
             alt="main-image"
-            className="w-full h-auto"
+            className="transition-transform duration-500 ease-in-out transform group-hover:scale-110 w-full h-[600px]"
             width={900}
             height={600}
           />
-          <p className="absolute bottom-[-2rem] left-0 right-0 opacity-0 transition-transform duration-300 ease-in-out group-hover:opacity-100 transform translate-y-[50%] text-7xl group-hover:translate-y-[-200%] text-white p-2">
+          <p className="absolute bottom-[-2rem] left-0 right-0 opacity-0 transition-transform duration-500 ease-in-out group-hover:opacity-100 transform translate-y-[50%] text-7xl group-hover:translate-y-[-50%] text-white p-2 w-48 ml-10">
             საქართველოს ბარმენთა ასოციაცია
           </p>
+          <Image
+            src="/logo.png"
+            alt="logo"
+            width={100}
+            height={100}
+            className="absolute top-10 left-10"
+          />
         </div>
-      </body>
-    </div>
+        <div className="flex flex-col grid-cols-8">
+          <Image
+            src="/ice-white.jpg"
+            alt="ice-white"
+            width={300}
+            height={300}
+            className="mb-4 h-[300px]"
+          />
+          <Image
+            src="/ice.jpg"
+            alt="ice"
+            width={300}
+            height={300}
+            className="h-[300px]"
+          />
+        </div>
+      </div>
+    </body>
   );
 };
 
