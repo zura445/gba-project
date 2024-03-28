7bad9674422a98049b854cf8bb56c964345a6329
zura tetrashvili <zuratetra@gmail.com>
2024-03-28 17:39:38 +0400

    I started doing the accordion

--- app/page/Main/Cocktail.tsx
+++ app/page/Main/Cocktail.tsx
@@ -1,28 +1,116 @@
-import React from "react";
-import categoriesData from "../../categories.json";
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import ArrowForwardIosSharpIcon from "@mui/icons-material/ArrowForwardIosSharp";
+import MuiAccordion, { AccordionProps } from "@mui/material/Accordion";
+import MuiAccordionSummary, {
+  AccordionSummaryProps,
+} from "@mui/material/AccordionSummary";
+import MuiAccordionDetails from "@mui/material/AccordionDetails";
+import Typography from "@mui/material/Typography";
 
-interface Category {
-  cocktailTitle?: string;
-  cocktailQuestion?: string;
-  cocktailAnsver?: string;
-}
+const Accordion = styled((props: AccordionProps) => (
+  <MuiAccordion disableGutters elevation={0} square {...props} />
+))(({ theme }) => ({
+  border: `1px solid ${theme.palette.divider}`,
+  "&:not(:last-child)": {
+    borderBottom: 0,
+  },
+  "&::before": {
+    display: "none",
+  },
+}));
 
-const Cocktail = () => {
-  if (categoriesData.length === 2) {
-    return null;
-  }
+const AccordionSummary = styled((props: AccordionSummaryProps) => (
+  <MuiAccordionSummary
+    expandIcon={<ArrowForwardIosSharpIcon sx={{ fontSize: "0.9rem" }} />}
+    {...props}
+  />
+))(({ theme }) => ({
+  backgroundColor:
+    theme.palette.mode === "dark"
+      ? "rgba(255, 255, 255, .05)"
+      : "rgba(0, 0, 0, .03)",
+  flexDirection: "row",
+  justifyContent: "space-between",
+  "& .MuiAccordionSummary-expandIconWrapper.Mui-expanded": {
+    transform: "none",
+  },
+  "& .MuiAccordionSummary-content": {
+    marginRight: theme.spacing(1),
+    display: "flex",
+    alignItems: "center",
+  },
+}));
 
-  const firstCategory: Category = categoriesData[2];
+const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
+  padding: theme.spacing(2),
+  borderTop: "1px solid rgba(0, 0, 0, .125)",
+}));
+
+export default function CustomizedAccordions() {
+  const [expanded, setExpanded] = React.useState<string | false>("panel1");
+
+  const handleChange =
+    (panel: string) => (event: React.SyntheticEvent, newExpanded: boolean) => {
+      setExpanded(newExpanded ? panel : false);
+    };
 
   return (
-    <section>
-      <h1 className="text-white text-3xl font-bold">
-        {firstCategory.cocktailTitle}
-      </h1>
-      <p className="text-white mt-4">{firstCategory.cocktailQuestion}</p>
-      <p className="text-white mt-2">{firstCategory.cocktailAnsver}</p>
-    </section>
+    <div>
+      <Accordion
+        expanded={expanded === "panel1"}
+        onChange={handleChange("panel1")}
+      >
+        <AccordionSummary aria-controls="panel1d-content" id="panel1d-header">
+          <Typography style={{ textAlign: "center", width: "100%" }}>
+            Collapsible Group Item #1
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography align="center">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum
+            dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada
+            lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion
+        expanded={expanded === "panel2"}
+        onChange={handleChange("panel2")}
+      >
+        <AccordionSummary aria-controls="panel1d-content" id="panel1d-header">
+          <Typography style={{ textAlign: "center", width: "100%" }}>
+            Collapsible Group Item #1
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography align="center">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum
+            dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada
+            lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion
+        expanded={expanded === "panel3"}
+        onChange={handleChange("panel3")}
+      >
+        <AccordionSummary aria-controls="panel1d-content" id="panel1d-header">
+          <Typography style={{ textAlign: "center", width: "100%" }}>
+            Collapsible Group Item #1
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography align="center">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum
+            dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada
+            lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+    </div>
   );
-};
-
-export default Cocktail;
+}
