9b153762b37cb30ef49227d34e5ff02a25d020ba
zura tetrashvili <zuratetra@gmail.com>
2024-03-29 17:24:43 +0400

    I added 3 cocktails and improved the cocktails page

--- app/page/Main/Cocktail.tsx
+++ app/page/Main/Cocktail.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import categoriesData from "../../categories.json";
-
+import Image from "next/image";
 import { styled } from "@mui/material/styles";
 import ArrowForwardIosSharpIcon from "@mui/icons-material/ArrowForwardIosSharp";
 import MuiAccordion, { AccordionProps } from "@mui/material/Accordion";
@@ -9,6 +9,8 @@ import MuiAccordionSummary, {
 } from "@mui/material/AccordionSummary";
 import MuiAccordionDetails from "@mui/material/AccordionDetails";
 import Typography from "@mui/material/Typography";
+
+import "./styles.css"; // Import external CSS file
 
 const Accordion = styled((props: AccordionProps) => (
   <MuiAccordion disableGutters elevation={0} square {...props} />
@@ -25,21 +27,14 @@ const AccordionSummary = styled((props: AccordionSummaryProps) => (
     {...props}
   />
 ))(({ theme }) => ({
-  backgroundColor:
-    theme.palette.mode === "dark"
-      ? "rgba(0, 0, 0, 0.95)"
-      : "rgba(0, 0, 0, 0.95)",
+  backgroundColor: "rgba(0, 0, 0, 0.95)",
   flexDirection: "row",
   justifyContent: "space-between",
-  "& .MuiAccordionSummary-expandIconWrapper.Mui-expanded": {},
-  "& .MuiAccordionSummary-content": {
-    marginRight: theme.spacing(2),
-  },
+  "& .MuiAccordionSummary-content": { marginRight: theme.spacing(2) },
 }));
 
 const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
   padding: theme.spacing(2),
-  borderTop: "1px solid rgba(0, 0, 0, .125)",
 }));
 
 const Cocktail = () => {
@@ -78,23 +73,41 @@ const Cocktail = () => {
         >
           <AccordionSummary aria-controls="panel1d-content" id="panel1d-header">
             <Typography
-              style={{ textAlign: "center", width: "100%" }}
-              className="text-white border-b-2 pb-2"
+              style={{ textAlign: "center", width: "100%", fontSize: "26px" }}
+              className="text-white border-b-2 pb-2 hover:text-red-500"
             >
-              ALEXANDER
+              ალექსანდრე (ALEXANDER)
             </Typography>
           </AccordionSummary>
-          <AccordionDetails
-            style={{ backgroundColor: "rgba(0, 0, 0, 0.95)" }}
-            className="text-white"
-          >
-            <Typography align="center">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-              eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-              eget.
-            </Typography>
+          <AccordionDetails className="accordion-details text-white">
+            <Image
+              src="/aleqsander.jpeg"
+              alt="aleqsander-cocktail"
+              width={300}
+              height={300}
+              className="h-[300px] w-[400px] object-cover mb-4"
+            />
+            <ol className="list-ol">
+              <li>Remy Martin(V.S.O.P) ან Armagnac - 30 მლ</li>
+              <li>ლიქიორი Crem de cacao (brown) - 30მლ</li>
+              <li>ნაღები - 30 მლ</li>
+            </ol>
+            <div className="mt-6">
+              <span className="font-bold">მეთოდი: </span>
+              <span>Shake & Strain</span>
+            </div>
+            <div>
+              <span className="font-bold">გარნირი: </span>
+              <span>გახეხილი ჯავზის კაკალი ბარის კოვზის მეოთხედი</span>
+            </div>
+            <div>
+              <span className="font-bold">ჭიქა: </span>
+              <span>კოქტეილის ბოკალი</span>
+            </div>
+            <div>
+              <span className="font-bold">კატეგორია: </span>
+              <span>კოქტეილი მთელი დღისთვის</span>
+            </div>
           </AccordionDetails>
         </Accordion>
         <Accordion
@@ -103,23 +116,44 @@ const Cocktail = () => {
         >
           <AccordionSummary aria-controls="panel1d-content" id="panel1d-header">
             <Typography
-              style={{ textAlign: "center", width: "100%" }}
-              className="text-white border-b-2 pb-2"
+              style={{ textAlign: "center", width: "100%", fontSize: "26px" }}
+              className="text-white border-b-2 pb-2 hover:text-red-500"
             >
-              Collapsible Group Item #1
+              ამერიკანო (AMERICANO)
             </Typography>
           </AccordionSummary>
           <AccordionDetails
             style={{ backgroundColor: "black" }}
             className="text-white"
           >
-            <Typography align="center">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-              eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-              eget.
-            </Typography>
+            <Image
+              src="/americano.jpg"
+              alt="americano-cocktail"
+              width={300}
+              height={300}
+              className="h-[300px] w-[400px] object-cover mb-4"
+            />
+            <ol className="list-ol">
+              <li>ბიტერი Campari Bitteri - 30 მლ</li>
+              <li>ვერმუტი Martini Rosso - 30მლ</li>
+              <li>სოდიანი წყალი</li>
+            </ol>
+            <div className="mt-6">
+              <span className="font-bold">მეთოდი: </span>
+              <span>Build</span>
+            </div>
+            <div>
+              <span className="font-bold">გარნირი: </span>
+              <span>ლიმნის რგოლის ნახევარი</span>
+            </div>
+            <div>
+              <span className="font-bold">ჭიქა: </span>
+              <span>Old fashioned</span>
+            </div>
+            <div>
+              <span className="font-bold">კატეგორია: </span>
+              <span>აპერიტივი</span>
+            </div>
           </AccordionDetails>
         </Accordion>
         <Accordion
@@ -128,122 +162,49 @@ const Cocktail = () => {
         >
           <AccordionSummary aria-controls="panel1d-content" id="panel1d-header">
             <Typography
-              style={{ textAlign: "center", width: "100%" }}
-              className="text-white border-b-2 pb-2"
+              style={{ textAlign: "center", width: "100%", fontSize: "26px" }}
+              className="text-white border-b-2 pb-2 hover:text-red-500"
             >
-              Collapsible Group Item #1
+              ანგელოზის სახე (ANGEL FACE)
             </Typography>
           </AccordionSummary>
           <AccordionDetails
             style={{ backgroundColor: "black" }}
             className="text-white"
           >
-            <Typography align="center">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-              eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-              eget.
-            </Typography>
+            <Image
+              src="/angel-face.jpg"
+              alt="angel-face-cocktail"
+              width={300}
+              height={300}
+              className="h-[300px] w-[400px] object-cover mb-4"
+            />
+            <ol className="list-ol">
+              <li>ჯინი Beefeater - 30 მლ</li>
+              <li>ლიქიორი Apricot Brand - 30მლ</li>
+              <li>Calvados - 30 მლ</li>
+            </ol>
+            <div className="mt-6">
+              <span className="font-bold">მეთოდი: </span>
+              <span>Shake & Strain</span>
+            </div>
+            <div>
+              <span className="font-bold">გარნირი: </span>
+              <span>ლიმნის ცედრა</span>
+            </div>
+            <div>
+              <span className="font-bold">ჭიქა: </span>
+              <span>კოქტეილის ბოკალი</span>
+            </div>
+            <div>
+              <span className="font-bold">კატეგორია: </span>
+              <span>კოქტეილი მთელი დღისთვის</span>
+            </div>
           </AccordionDetails>
         </Accordion>
       </div>
     </>
   );
 };
+
 export default Cocktail;
-
-// import React, { useState } from "react";
-// import categoriesData from "../../categories.json";
-// import Accordion, { AccordionSlots } from "@mui/material/Accordion";
-// import AccordionSummary from "@mui/material/AccordionSummary";
-// import AccordionDetails from "@mui/material/AccordionDetails";
-// import Typography from "@mui/material/Typography";
-// import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-// import Fade from "@mui/material/Fade";
-
-// interface Category {
-//   cocktailTitle?: string;
-//   cocktailQuestion?: string;
-//   cocktailAnsver?: string;
-// }
-
-// const Cocktail = () => {
-//   const [expanded, setExpanded] = useState(false);
-
-//   if (categoriesData.length === 2) {
-//     return null;
-//   }
-
-//   const firstCategory: Category = categoriesData[2];
-
-//   const handleExpansion = () => {
-//     setExpanded((prevExpanded) => !prevExpanded);
-//   };
-
-//   return (
-//     <>
-//       <section>
-//         <h1 className="text-white text-3xl font-bold">
-//           {firstCategory.cocktailTitle}
-//         </h1>
-//         <p className="text-white mt-4">{firstCategory.cocktailQuestion}</p>
-//         <p className="text-white mt-2">{firstCategory.cocktailAnsver}</p>
-//       </section>
-//       <section>
-//         <div>
-//           <Accordion
-//             className="mt-6"
-//             expanded={expanded}
-//             onChange={handleExpansion}
-//             slots={{ transition: Fade as AccordionSlots["transition"] }}
-//             slotProps={{ transition: { timeout: 400 } }}
-//             sx={{
-//               backgroundColor: "black",
-//               "& .MuiAccordion-region": { height: expanded ? "auto" : 0 },
-//               "& .MuiAccordionDetails-root": {
-//                 display: expanded ? "block" : "none",
-//               },
-//             }}
-//           >
-//             <AccordionSummary
-//               expandIcon={<ExpandMoreIcon className="text-white" />}
-//               aria-controls="panel1-content"
-//               id="panel1-header"
-//             >
-//               <Typography className="text-white">
-//                 Custom transition using Fade
-//               </Typography>
-//             </AccordionSummary>
-//             <AccordionDetails>
-//               <Typography className="text-white">
-//                 Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-//                 Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-//                 eget.
-//               </Typography>
-//             </AccordionDetails>
-//           </Accordion>
-
-//           <Accordion sx={{ backgroundColor: "black" }}>
-//             <AccordionSummary
-//               expandIcon={<ExpandMoreIcon className="text-white" />}
-//               aria-controls="panel2-content"
-//               id="panel2-header"
-//             >
-//               <Typography className="text-white">
-//                 Default transition using Collapse
-//               </Typography>
-//             </AccordionSummary>
-//             <AccordionDetails>
-//               <Typography className="text-white">
-//                 Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-//                 Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-//                 eget.
-//               </Typography>
-//             </AccordionDetails>
-//           </Accordion>
-//         </div>
-//       </section>
-//     </>
-//   );
-// };
