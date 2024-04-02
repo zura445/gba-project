cf8c6a0f400be358ee6617c2c3fc6e6a3c0b79b1
zura tetrashvili <zuratetra@gmail.com>
2024-04-02 21:22:24 +0400

    cocktail page map

--- app/components/Modal.tsx
+++ app/components/Modal.tsx
@@ -34,15 +34,30 @@ const Modal: React.FC<ModalProps> = ({ closeModal, isOpen, children }) => {
               leaveFrom="opacity-100 scale-100"
               leaveTo="opacity-0 scale-95"
             >
-              <Dialog.Panel className="w-full md:max-w-[60%] max-w-[90%] h-[86%] md:h-[60%]  bg-black rounded-lg absolute">
+              <Dialog.Panel className="w-full md:max-w-[60%] max-w-[90%] h-[86%] md:h-[60%] bg-black rounded-lg absolute">
                 <button
                   type="button"
-                  className="rounded-md bg-black px-2 py-2 text-white top-2 left-36 relative md:hidden"
+                  className="rounded-md bg-black px-2 pb-3 text-white top-2 left-36 relative md:hidden"
                   onClick={closeModal}
+                  title="Close Modal" // Add a title attribute with a meaningful description
                 >
-                  X
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
                 </button>
-                <div className="overflow-y-scroll scrollbar-thin scrollbar-thumb-red-800 scrollbar-track-gray-100 w-full h-[600px] transform rounded-2xl bg-black p-6 text-left">
+
+                <div className="overflow-y-scroll scrollbar-thin scrollbar-thumb-red-800 scrollbar-track-gray-800 w-full h-[600px] transform rounded-2xl bg-black p-6 text-left">
                   {children}
                 </div>
               </Dialog.Panel>
--- app/page/Main/Cocktail.tsx
+++ app/page/Main/Cocktail.tsx
@@ -10,7 +10,7 @@ import MuiAccordionSummary, {
 import MuiAccordionDetails from "@mui/material/AccordionDetails";
 import Typography from "@mui/material/Typography";
 
-import "./styles.css"; // Import external CSS file
+import "./styles.css";
 
 const Accordion = styled((props: AccordionProps) => (
   <MuiAccordion disableGutters elevation={0} square {...props} />
@@ -46,14 +46,91 @@ const Cocktail = () => {
     };
 
   interface Category {
-    cocktailTitle?: string;
+    categoryTitle?: string;
     cocktailQuestion?: string;
     cocktailAnsver?: string;
+    cocktailTitle?: string;
+    ingredient1?: string;
+    ingredient2?: string;
+    ingredient3?: string;
+    method?: string;
+    methodResponse?: string;
+    garnish?: string;
+    garnishResponse?: string;
+    glass?: string;
+    glassResponse?: string;
+    category?: string;
+    categoryResponse?: string;
   }
 
   if (categoriesData.length === 2) {
     return null;
   }
+  const coctailList = [
+    {
+      catTitle: "traki",
+      catName: "duma",
+      name: "(ალექსანდრე) ALEXANDER",
+      Image: "aleqsander.jpeg",
+      altName: "aleqsander-cocktail",
+      ingredients: [
+        { value: "Remy Martin(V.S.O.P) ან Armagnac - 30 მლ" },
+        { value: "ლიქიორი Crem de cacao (brown) - 30მლ" },
+        { value: "ნაღები - 30 მლ" },
+      ],
+      method: "მეთოდი: ",
+      methodResponse: "Shake & Strain",
+      garnish: "გარნირი: ",
+      garnishResponse: "გახეხილი ჯავზის კაკალი ბარის კოვზის მეოთხედი",
+      glass: "ჭიქა: ",
+      glassResponse: "კოქტეილის ბოკალი",
+      category: "კატეგორია: ",
+      categoryResponse: "კოქტეილი მთელი დღისთვის",
+      modalComponent: "cocktailComponent",
+    },
+    {
+      name: "ამერიკანო (AMERICANO)",
+      Image: "americano.jpg",
+      altName: "aleqsander-cocktail",
+      ingredients: [
+        { value: "Remy Martin(V.S.O.P) ან Armagnac - 30 მლ" },
+        { value: "ლიქიორი Crem de cacao (brown) - 30მლ" },
+        { value: "ნაღები - 30 მლ" },
+      ],
+      method: "მეთოდი: ",
+      methodResponse: "Shake & Strain",
+      garnish: "გარნირი: ",
+      garnishResponse: "გახეხილი ჯავზის კაკალი ბარის კოვზის მეოთხედი",
+      glass: "ჭიქა: ",
+      glassResponse: "კოქტეილის ბოკალი",
+      category: "კატეგორია: ",
+      categoryResponse: "კოქტეილი მთელი დღისთვის",
+      modalComponent: "cocktailComponent",
+    },
+    {
+      name: "ამერიკანო (AMERICANO)",
+      Image: "americano.jpg",
+      altName: "aleqsander-cocktail",
+      ingredients: [
+        { value: "Remy Martin(V.S.O.P) ან Armagnac - 30 მლ" },
+        { value: "ლიქიორი Crem de cacao (brown) - 30მლ" },
+        { value: "ნაღები - 30 მლ" },
+      ],
+      method: "მეთოდი: ",
+      methodResponse: "Shake & Strain",
+      garnish: "გარნირი: ",
+      garnishResponse: "გახეხილი ჯავზის კაკალი ბარის კოვზის მეოთხედი",
+      glass: "ჭიქა: ",
+      glassResponse: "კოქტეილის ბოკალი",
+      category: "კატეგორია: ",
+      categoryResponse: "კოქტეილი მთელი დღისთვის",
+      modalComponent: "cocktailComponent",
+    },
+    {
+      title: "title g",
+      name: "vaxo",
+    },
+  ];
 
   const firstCategory: Category = categoriesData[2];
 
@@ -61,56 +138,69 @@ const Cocktail = () => {
     <>
       <section>
         <h1 className="text-white text-3xl font-bold">
-          {firstCategory.cocktailTitle}
+          {firstCategory.categoryTitle}
         </h1>
-        <p className="text-white mt-4">{firstCategory.cocktailQuestion}</p>
-        <p className="text-white mt-2">{firstCategory.cocktailAnsver}</p>
+        <p className="text-white mt-4">{coctailList[0].catName}</p>
+        <p className="text-white mt-2">{coctailList[0].catTitle}</p>
       </section>
       <div className="mt-6">
-        <Accordion
-          expanded={expanded === "panel1"}
-          onChange={handleChange("panel1")}
-        >
-          <AccordionSummary aria-controls="panel1d-content" id="panel1d-header">
-            <Typography
-              style={{ textAlign: "center", width: "100%", fontSize: "26px" }}
-              className="text-white border-b-2 pb-2 hover:text-red-500"
+        {coctailList.map((item, i) => {
+          return (
+            <Accordion
+              key={i}
+              expanded={expanded === "panel1"}
+              onChange={handleChange("panel1")}
             >
-              ალექსანდრე (ALEXANDER)
-            </Typography>
-          </AccordionSummary>
-          <AccordionDetails className="accordion-details text-white">
-            <Image
-              src="/aleqsander.jpeg"
-              alt="aleqsander-cocktail"
-              width={300}
-              height={300}
-              className="h-[300px] w-[400px] object-cover mb-4"
-            />
-            <ol className="list-ol">
-              <li>Remy Martin(V.S.O.P) ან Armagnac - 30 მლ</li>
-              <li>ლიქიორი Crem de cacao (brown) - 30მლ</li>
-              <li>ნაღები - 30 მლ</li>
-            </ol>
-            <div className="mt-6">
-              <span className="font-bold">მეთოდი: </span>
-              <span>Shake & Strain</span>
-            </div>
-            <div>
-              <span className="font-bold">გარნირი: </span>
-              <span>გახეხილი ჯავზის კაკალი ბარის კოვზის მეოთხედი</span>
-            </div>
-            <div>
-              <span className="font-bold">ჭიქა: </span>
-              <span>კოქტეილის ბოკალი</span>
-            </div>
-            <div>
-              <span className="font-bold">კატეგორია: </span>
-              <span>კოქტეილი მთელი დღისთვის</span>
-            </div>
-          </AccordionDetails>
-        </Accordion>
-        <Accordion
+              <AccordionSummary
+                aria-controls="panel1d-content"
+                id="panel1d-header"
+              >
+                <Typography
+                  style={{
+                    textAlign: "center",
+                    width: "100%",
+                    fontSize: "26px",
+                  }}
+                  className="text-white border-b-2 pb-2 hover:text-red-500"
+                >
+                  {item.name}
+                </Typography>
+              </AccordionSummary>
+              <AccordionDetails className="accordion-details text-white">
+                <Image
+                  src={`/${item.Image}`}
+                  alt={`${item.altName}`}
+                  width={300}
+                  height={300}
+                  className="h-[300px] w-[400px] object-cover mb-4"
+                />
+                <ol className="list-ol">
+                  {item.ingredients?.map((item, i) => {
+                    return <li key={i}>{item.value} </li>;
+                  })}
+                </ol>
+                <div className="mt-6">
+                  <span className="font-bold">{item.method}</span>
+                  <span>{item.methodResponse}</span>
+                </div>
+                <div>
+                  <span className="font-bold">{item.garnish}</span>
+                  <span>{item.garnishResponse}</span>
+                </div>
+                <div>
+                  <span className="font-bold">{item.glass}</span>
+                  <span>{item.glassResponse}</span>
+                </div>
+                <div>
+                  <span className="font-bold">{item.category}</span>
+                  <span>{item.categoryResponse}</span>
+                </div>
+              </AccordionDetails>
+            </Accordion>
+          );
+        })}
+
+        {/* <Accordion
           expanded={expanded === "panel2"}
           onChange={handleChange("panel2")}
         >
@@ -201,7 +291,7 @@ const Cocktail = () => {
               <span>კოქტეილი მთელი დღისთვის</span>
             </div>
           </AccordionDetails>
-        </Accordion>
+        </Accordion> */}
       </div>
     </>
   );
