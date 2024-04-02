9d5beca49fc06b3a241a3c0ef14b72cb3628707d
zura tetrashvili <zuratetra@gmail.com>
2024-04-02 22:51:35 +0400

    accordion bug fixing

--- app/page/Main/Cocktail.tsx
+++ app/page/Main/Cocktail.tsx
@@ -38,11 +38,11 @@ const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
 }));
 
 const Cocktail = () => {
-  const [expanded, setExpanded] = React.useState<string | false>("panel1");
+  const [expanded, setExpanded] = React.useState<string | false>(false);
 
   const handleChange =
-    (panel: string) => (event: React.SyntheticEvent, newExpanded: boolean) => {
-      setExpanded(newExpanded ? panel : false);
+    (panel: string) => (event: React.SyntheticEvent, isExpanded: boolean) => {
+      setExpanded(isExpanded ? panel : false);
     };
 
   interface Category {
@@ -68,8 +68,9 @@ const Cocktail = () => {
   }
   const coctailList = [
     {
-      catTitle: "traki",
-      catName: "duma",
+      catTitle: "რას ნიშნავს კლასიკური კოქტეილი?",
+      catName:
+        "ეს არის (IBA-ს) მსოფლიო ბარმენთა ასოციაციის მიერ აღიარებული კოქტეილები",
       name: "(ალექსანდრე) ALEXANDER",
       Image: "aleqsander.jpeg",
       altName: "aleqsander-cocktail",
@@ -148,12 +149,12 @@ const Cocktail = () => {
           return (
             <Accordion
               key={i}
-              expanded={expanded === "panel1"}
-              onChange={handleChange("panel1")}
+              expanded={expanded === `panel${i}`}
+              onChange={handleChange(`panel${i}`)}
             >
               <AccordionSummary
-                aria-controls="panel1d-content"
-                id="panel1d-header"
+                aria-controls={`panel${i}-content`}
+                id={`panel${i}-header`}
               >
                 <Typography
                   style={{
@@ -175,8 +176,8 @@ const Cocktail = () => {
                   className="h-[300px] w-[400px] object-cover mb-4"
                 />
                 <ol className="list-ol">
-                  {item.ingredients?.map((item, i) => {
-                    return <li key={i}>{item.value} </li>;
+                  {item.ingredients?.map((ingredient, index) => {
+                    return <li key={index}>{ingredient.value}</li>;
                   })}
                 </ol>
                 <div className="mt-6">
@@ -199,99 +200,6 @@ const Cocktail = () => {
             </Accordion>
           );
         })}
-
-        {/* <Accordion
-          expanded={expanded === "panel2"}
-          onChange={handleChange("panel2")}
-        >
-          <AccordionSummary aria-controls="panel1d-content" id="panel1d-header">
-            <Typography
-              style={{ textAlign: "center", width: "100%", fontSize: "26px" }}
-              className="text-white border-b-2 pb-2 hover:text-red-500"
-            >
-              ამერიკანო (AMERICANO)
-            </Typography>
-          </AccordionSummary>
-          <AccordionDetails
-            style={{ backgroundColor: "black" }}
-            className="text-white"
-          >
-            <Image
-              src="/americano.jpg"
-              alt="americano-cocktail"
-              width={300}
-              height={300}
-              className="h-[300px] w-[400px] object-cover mb-4"
-            />
-            <ol className="list-ol">
-              <li>ბიტერი Campari Bitteri - 30 მლ</li>
-              <li>ვერმუტი Martini Rosso - 30მლ</li>
-              <li>სოდიანი წყალი</li>
-            </ol>
-            <div className="mt-6">
-              <span className="font-bold">მეთოდი: </span>
-              <span>Build</span>
-            </div>
-            <div>
-              <span className="font-bold">გარნირი: </span>
-              <span>ლიმნის რგოლის ნახევარი</span>
-            </div>
-            <div>
-              <span className="font-bold">ჭიქა: </span>
-              <span>Old fashioned</span>
-            </div>
-            <div>
-              <span className="font-bold">კატეგორია: </span>
-              <span>აპერიტივი</span>
-            </div>
-          </AccordionDetails>
-        </Accordion>
-        <Accordion
-          expanded={expanded === "panel3"}
-          onChange={handleChange("panel3")}
-        >
-          <AccordionSummary aria-controls="panel1d-content" id="panel1d-header">
-            <Typography
-              style={{ textAlign: "center", width: "100%", fontSize: "26px" }}
-              className="text-white border-b-2 pb-2 hover:text-red-500"
-            >
-              ანგელოზის სახე (ANGEL FACE)
-            </Typography>
-          </AccordionSummary>
-          <AccordionDetails
-            style={{ backgroundColor: "black" }}
-            className="text-white"
-          >
-            <Image
-              src="/angel-face.jpg"
-              alt="angel-face-cocktail"
-              width={300}
-              height={300}
-              className="h-[300px] w-[400px] object-cover mb-4"
-            />
-            <ol className="list-ol">
-              <li>ჯინი Beefeater - 30 მლ</li>
-              <li>ლიქიორი Apricot Brand - 30მლ</li>
-              <li>Calvados - 30 მლ</li>
-            </ol>
-            <div className="mt-6">
-              <span className="font-bold">მეთოდი: </span>
-              <span>Shake & Strain</span>
-            </div>
-            <div>
-              <span className="font-bold">გარნირი: </span>
-              <span>ლიმნის ცედრა</span>
-            </div>
-            <div>
-              <span className="font-bold">ჭიქა: </span>
-              <span>კოქტეილის ბოკალი</span>
-            </div>
-            <div>
-              <span className="font-bold">კატეგორია: </span>
-              <span>კოქტეილი მთელი დღისთვის</span>
-            </div>
-          </AccordionDetails>
-        </Accordion> */}
       </div>
     </>
   );
