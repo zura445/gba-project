5d52d65372ce298158214ac488a7ad4f2095cda3
zura tetrashvili <zuratetra@gmail.com>
2024-09-02 20:53:48 +0400

    text fix

--- app/page/Main/modalComponents/News.tsx
+++ app/page/Main/modalComponents/News.tsx
@@ -16,18 +16,30 @@ const News = () => {
   const firstCategory: Category = categoriesData[0];
 
   return (
-    <section>
-      <h1 className="text-3xl text-white">{firstCategory.modalTitle}</h1>
-      <p className="mt-6 text-white">{firstCategory.modalDescription}</p>
-      <ul
-        role="list"
-        className="marker:text-red-700 list-disc pl-5 space-y-3 mt-4"
-      >
-        <li className="text-white">{firstCategory.modalSteoOne}</li>
-        <li className="text-white">{firstCategory.modalStepTwo}</li>
-        <li className="text-white">{firstCategory.modalStepThree}</li>
-      </ul>
-    </section>
+    <>
+      <section>
+        <h1 className="text-3xl text-white">
+          2024 წლის მსოფლიოს, ევროპის და საქართველოს ჩემპიონატები
+        </h1>
+        <p className="mt-6 text-white">დეტალები:</p>
+        <ul
+          role="list"
+          className="marker:text-red-700 list-disc pl-5 space-y-3 mt-4"
+        >
+          <li className="text-white">
+            2024 წლის 31 ოქტომბრიდან 4 ნოემბრის ჩათვლით, პორტუგალიაში კუნძულ
+            მადეირაზე ჩატარდება მსოფლიო ჩემპიონატი კლასიკასა და ფლაერინგში
+          </li>
+          <li className="text-white">
+            2. 2024 წლის დეკემბერში ჩატარდება საქართველოს ჩემპიონატში კლასიკასა
+            და ფლაერინგში
+          </li>
+          <li className="text-white">
+            3. 2024 წლის ევროპის ჩემპიონატი არ ჩატარდება
+          </li>
+        </ul>
+      </section>
+    </>
   );
 };
 
