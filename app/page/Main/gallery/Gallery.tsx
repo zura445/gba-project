608cf9e3ee448480ec0101ce8ee8b6a9a6496770
zura tetrashvili <zuratetra@gmail.com>
2024-12-23 12:27:02 +0400

    delete log

--- app/page/Main/gallery/Gallery.tsx
+++ app/page/Main/gallery/Gallery.tsx
@@ -8,7 +8,6 @@ import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/scrollbar";
 import "./Gallery.css";
-import { log } from "console";
 
 const Gallery = () => {
   const [showGallery, setShowGallery] = useState(false);
